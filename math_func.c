22712a2684cee486566cf46cb3643d87671477ce
kkoolwal <kushalkoolwal@gmail.com>
2013-07-06 15:22:54 -0700

    Added power function.

--- math_func.c
+++ math_func.c
@@ -4,10 +4,21 @@
 #include<ctype.h>
 
 
+int mypow (int base, int power) {
+
+	int i,result=1;
+	
+	for (i = 1; i <= power; i++)
+		result=result*base;
+
+	return result;
+}
+
 void display_menu () {
 
 	printf("\n***Math functions in C***\n");
-	printf("Press 0  to display this menu\n");	
+	printf("Press 0  to display this menu\n");
+	printf("Press 1  to calculate power\n");
 	printf("Press -1 to quit\n");
 
 }
@@ -16,20 +27,28 @@ void display_menu () {
 int main (void) {
 
 	int command;
-	
+	int base, power=0;
+
+	display_menu ();
 	printf("Enter a command from above menu:");
 	scanf("%d", &command);
+
 	switch (command) {
 	
 		case 0:
 			display_menu();
 			break;
+		case 1:
+			printf("Enter base:");
+			scanf("%d", &base);
+			printf("Enter power:");
+			scanf("%d", &power);
+			printf("The pow(base,power) is %d\n", mypow(base, power));
 		case -1:
 			return 0;
 		default:
 			printf("Invalid choice\n");
 			break;
 	}
-	printf("\n");
-	printf("\n");
+	return 0;
 }
