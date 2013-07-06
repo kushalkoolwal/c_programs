574b1b01cce6b09078750abb7726231470a72057
kkoolwal <kushalkoolwal@gmail.com>
2013-07-06 13:48:33 -0700

    Added word counting functionality.

--- wc.c
+++ wc.c
@@ -1,8 +1,14 @@
-/* UNIX word count (wc) program */
+/* UNIX word count (wc) program
+   Word is defined here as sequence of characters not containing a
+   blank, tab or newline.
+ */
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include<ctype.h>
+
+#define IN 		1	/*inside the word */
+#define OUT		0	/*outside the word */
 
 /*  EOF is defined as -1 on this platform.
 	'int' instead of 'char' is used to detect end of input (EOF in this case).
@@ -16,18 +22,29 @@
 
 int main (void) {
 
-	int c;
-	long nl,nc;
+	int c, word_status;
+	long nl,nc,nw;
 	
-	nl=nc=0;
+	/* assignments associate from right to left*/
+	nl=nc=nw=0;
+	word_status=OUT;
 
+	/* keep reading until EOF is encountered */
 	while ((c=getchar()) != EOF) {
+		nc++; 			/* count character immediately */
 		if (c == '\n')
-			nl++;
-		nc++;
+			nl++;		/* count lines as soon we encounter '\n' */
+		if (c == ' ' || c == '\n' || c == 't')
+			word_status=OUT;			
+		else if (word_status == OUT) {
+			word_status=IN;
+			nw++;	
+		}
 	}
+
 	printf("No. of characters:%ld\n", nc);
 	printf("No. of lines:%ld\n", nl);
+	printf("No. of words:%ld\n", nw);
 
 	return 0;
 }
