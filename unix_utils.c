0e3ed3c4be1b7786db0a68084ef124d88ca39a6c
kkoolwal <kushalkoolwal@gmail.com>
2013-07-06 12:49:25 -0700

    Added printing of no. of characters and no. of lines to the echo program.

--- unix_utils.c
+++ unix_utils.c
@@ -8,13 +8,25 @@
 	'int' instead of 'char' is used to detect end of input.
     'char' can't be used because 'c' should be big enough to hold EOF
     in addition to any possible char values and hence 'int' is used.
+	Note that input is buffered so you have to hit ENTER key for getchar to
+	fetch characters from the buffer.	
+	Pressing ENTER key == '\n'
+	Pressing Ctrl+D == EOF
  */
 void copy () {
 
 	int c;
+	long nl,nc;
+	
+	nl=nc=0;
 
-	while ((c=getchar())!=EOF)
-		putchar(c);
+	while ((c=getchar()) != EOF) {
+		if (c == '\n')
+			nl++;
+		nc++;
+	}
+	printf("No. of characters:%ld\n", nc);
+	printf("No. of lines:%ld\n", nl);
 }
 
 void display_menu () {
@@ -28,8 +40,8 @@ void display_menu () {
 
 int main (void) {
 
-	int command;	
-	
+	int command;
+
 	display_menu ();
 	printf("Enter a command from above menu:");
 	scanf("%d", &command);
