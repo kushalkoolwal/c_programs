1bd25cae2689178a4b37c9115970b017ccb1efe7
kkoolwal <kushalkoolwal@gmail.com>
2013-07-09 12:55:21 -0700

    Added string copy function (non-pointer) version.

--- string_func.c
+++ string_func.c
@@ -10,6 +10,19 @@
 /* define exit code */
 #define CODE 0
 
+#define ARRAY_SIZE 15
+
+/* string copy function */
+void mystrcpy (char d[], char s[]) {
+
+	int i=0;
+	
+	while (s[i] != '\0') {
+		d[i] = s[i];
+		i++;
+	}
+}
+
 void display_menu () {
 
 	printf("\n***String based programs in C***\n");
@@ -19,21 +32,26 @@ void display_menu () {
 
 }
 
-
 int main (void) {
 
 	int command;
-	
+
+	char buffer[ARRAY_SIZE];
+	char output[ARRAY_SIZE];
+
+	display_menu ();
 	printf("Enter a command from above menu:");
 	scanf("%d", &command);
-	switch (command) {
-	
+
+	switch (command) {	
 		case 0:
 			display_menu();
 			break;
 		case 1:
 			printf("Enter a string to copy:");
-			scanf("%d", &num);			
+			scanf("%s", buffer);
+			mystrcpy(output, buffer);
+			printf("Source: %s, Dest: %s\n", buffer, output);
 			break;
 		case -1:
 			return 0;
@@ -43,4 +61,5 @@ int main (void) {
 	}
 	printf("\n");
 	printf("\n");
+	return 0;
 }
