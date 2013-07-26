dba29b56f59b6fb1af2fafc5d3eb0ed1ff5b0a07
kkoolwal <kushalkoolwal@gmail.com>
2013-07-26 08:33:19 -0700

    Added some comments.

--- ll_func.c
+++ ll_func.c
@@ -18,7 +18,30 @@ typedef struct node {
 	struct node *next; /* next is a pointer to a structure of type node_t */
 } node_t;
 
-node_t *head, *new_node, *temp, *temp2;
+node_t *head, *new_node, *temp, *temp2, *dup;
+
+void remove_duplicates() {
+
+	temp = head;
+
+	if (temp == NULL) {
+		printf("List is empty\n");
+		return;
+	}
+
+	while (temp != NULL) {
+		temp2 = temp;
+		while (temp2 != NULL && temp2->next != NULL) {
+			if (temp->data == temp2->next->data) {
+				dup = temp2->next;
+				temp2->next=temp2->next->next;
+				free (dup);
+			}
+			temp2=temp2->next;
+		}
+		temp=temp->next;
+	}
+}
 
 void delete_node () {
 
@@ -96,6 +119,7 @@ void create_list() {
 	else {
 		/* append node at the end of list */
 		temp = head;
+		/* temp->next locates the last node */
 		while(temp->next != NULL)
 			temp=temp->next;
 		temp->next=new_node;
@@ -108,7 +132,8 @@ void display_menu () {
 	printf("Press 0 to display this menu\n");	
 	printf("Press 1 to create list\n");
 	printf("Press 2 to display list\n");
-	printf("Press 3 to delete list\n");
+	printf("Press 3 to delete node\n");
+	printf("Press 4 to remove duplicate nodes\n");
 	printf("Press -1 to quit\n");
 
 }
@@ -137,9 +162,11 @@ int main (void) {
 		case 3:
 			delete_node();
 			break;
+		case 4:
+			remove_duplicates();
+			break;
 		case -1:
 			return 0;
-
 		default:		
 			printf("Please enter a valid choice\n");
 			display_menu();
